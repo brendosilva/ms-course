a799e5797202e2a8fa605be1de8336cd8be0b817
Brendo Silva <breendo.01@gmail.com>
2022-06-22 15:03:00 -0300

    hr-worker

--- src/main/java/com/devsuperior/hrworker/repositories/WorkerRepository.java
+++ src/main/java/com/devsuperior/hrworker/repositories/WorkerRepository.java
@@ -1,9 +1,9 @@
 package com.devsuperior.hrworker.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.devsuperior.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
-
 }
